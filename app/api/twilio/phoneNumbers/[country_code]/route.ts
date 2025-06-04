5e29ae07d7e479e2cc87ce34a4670d34128d8669
Mohit Bhalotia <mohitkumarbhalotia420@gmail.com>
2025-06-04 13:32:35 +0530

    final fix

--- app/api/twilio/phoneNumbers/[country_code]/route.ts
+++ app/api/twilio/phoneNumbers/[country_code]/route.ts
@@ -1,22 +1,23 @@
 import twilio from "twilio";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { country_code: string } }
-) {
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const pathParts = url.pathname.split("/");
+  const country_code = pathParts[pathParts.length - 1];
+
   const client = twilio(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_AUTH_TOKEN
   );
 
-  console.log(params.country_code);
+  console.log(country_code);
 
   const availablePhoneNumberLocal = await client
-    .availablePhoneNumbers(params.country_code)
+    .availablePhoneNumbers(country_code)
     .local.list({ voiceEnabled: true, limit: 10 });
 
   const availablePhoneNumberTollFree = await client
-    .availablePhoneNumbers(params.country_code)
+    .availablePhoneNumbers(country_code)
     .tollFree.list({ voiceEnabled: true, limit: 10 });
 
   return Response.json({
